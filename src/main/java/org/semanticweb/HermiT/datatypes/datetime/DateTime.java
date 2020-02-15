60105535edd0c0f79948fbe99c5abda46d0b2e29
ignazio <ignazio.palmisano@gmail.com>
2020-02-15 15:56:29 +0000

    Fix DateTime parsing error in HermiT #5

--- src/main/java/org/semanticweb/HermiT/datatypes/datetime/DateTime.java
+++ src/main/java/org/semanticweb/HermiT/datatypes/datetime/DateTime.java
@@ -38,7 +38,7 @@ public class DateTime {
         ":"+
         TWO_DIGITS+
         ":"+
-        TWO_DIGITS+"([.]([0-9]{1,3}))?"+
+        TWO_DIGITS+"([.]([0-9]{1,3})[0-9]*)?"+
         "((Z)|(([+]|-)"+TWO_DIGITS+":"+TWO_DIGITS+"))?"
     );
     // according to XML Schema 1.1 spec (http://www.w3.org/TR/xmlschema11-2/#dateTime) the reg exp is as follows:
@@ -149,7 +149,7 @@ public class DateTime {
                 day=daysInMonth(year,month);
             }
         }
-        StringBuffer buffer=new StringBuffer();
+        StringBuilder buffer=new StringBuilder();
         appendPadded(buffer,year,4);
         buffer.append('-');
         appendPadded(buffer,month,2);
@@ -204,7 +204,7 @@ public class DateTime {
     public int getTimeZoneOffset() {
         return m_timeZoneOffset;
     }
-    protected void appendPadded(StringBuffer buffer,int value,int digits) {
+    protected void appendPadded(StringBuilder buffer,int value,int digits) {
         if (value<0)
             buffer.append('-');
         String stringAbsValue=String.valueOf(Math.abs(value));
